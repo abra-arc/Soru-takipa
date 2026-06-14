bcdab3164b330b528bc1b978865d623a919689f1
abra-arc <aysegulaygar8@gmail.com>
2026-06-14 14:28:24 +0300

    Update ai_studio_code.py

--- ai_studio_code.py
+++ ai_studio_code.py
@@ -218,22 +218,22 @@ if "DERS:" in cevap:
     except:
         pass
         
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
- 
-    tarih = datetime.now().strftime(
-    "%d.%m.%Y %H:%M"
+conn = sqlite3.connect(DB_FILE)
+cursor = conn.cursor()
+
+tarih = datetime.now().strftime(
+"%d.%m.%Y %H:%M"
 )
 
-    dosya_adi = f"{int(datetime.now().timestamp())}.png"
-    dosya_yolu = os.path.join(
-    IMAGE_DIR,
-    dosya_adi
+dosya_adi = f"{int(datetime.now().timestamp())}.png"
+dosya_yolu = os.path.join(
+IMAGE_DIR,
+dosya_adi
 )
 
-    image.save(dosya_yolu)
+image.save(dosya_yolu)
 
-    cursor.execute("""
+cursor.execute("""
 INSERT INTO sorular
 (ders, gorsel_yolu, cozum_metni, tarih)
 VALUES (?, ?, ?, ?)
@@ -245,13 +245,13 @@ VALUES (?, ?, ?, ?)
     tarih
 ))
 
-    conn.commit()
-    conn.close()
+conn.commit()
+conn.close()
 
-    st.success("Çözüm hazır!")
+st.success("Çözüm hazır!")
 
-    st.markdown("## 📦 Çözüm")
-    st.write(response.text)
+st.markdown("## 📦 Çözüm")
+st.write(response.text)
     
                 
 
